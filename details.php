dc1bed7b79b7a09a640bf9c1d48536cf22692314
Vitez <p_vitez@fanshawec.ca>
2018-01-25 09:41:28 -0500

    init commit

---
+++ details.php
@@ -0,0 +1,12 @@
+<?php
+	
+?>
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Details</title>
+</head>
+<body>	
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,9 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Welcome to the Finest Selection of Blu-rays on the internets!</title>
+</head>
+<body>
+</body>
+</html>
