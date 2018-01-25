3a71c80097b2acef2adfcc201e10c8afb34a44d4
Vitez <p_vitez@fanshawec.ca>
2018-01-25 10:23:36 -0500

    add general queryall and display thumbs

--- admin/phpscripts/read.php
+++ admin/phpscripts/read.php
@@ -1,3 +1,18 @@
 <?php
-	
+
+	//Get all of something
+	 function getAll($tbl){
+		 include('connect.php');
+		 $queryAll = "SELECT * FROM {$tbl}";
+		 $runAll = mysqli_query($link, $queryAll);
+		 if($runAll){  //check if object exsists
+			 return $runAll;
+		 }else{
+			 $error = "We can't find the movies right now";
+			 return $error;
+		 }
+
+		 mysqli_close($link);
+	 }
+
 ?>
--- index.php
+++ index.php
@@ -1,3 +1,13 @@
+<?php
+  // ini_set('display errors',1);
+  // error_reporting(E_ALL);
+
+  require_once('admin/phpscripts/config.php');
+  $tbl = 'tbl_movies';
+  $getMovies = getAll($tbl);
+
+
+ ?>
 <!doctype html>
 <html>
 <head>
@@ -5,5 +15,19 @@
 <title>Welcome to the Finest Selection of Blu-rays on the internets!</title>
 </head>
 <body>
+  <?php
+  include('includes/nav.html');
+
+  if(!is_string($getMovies)){
+    while($row = mysqli_fetch_array($getMovies)){
+      echo "<img src=\"images/{$row['movies_cover']}\" alt=\"{$row['movies_title']}\">";
+    }
+  }else{
+    echo "<p class= \" error\">{$getMovies}</p>";
+  }
+
+  include('includes/footer.html');
+
+   ?>
 </body>
 </html>
