22cd1c68144cfbdf9b4abbd5967730625d89ef50
Vitez <p_vitez@fanshawec.ca>
2018-02-01 10:22:16 -0500

    finish genre filter and fixed small issues

--- admin/phpscripts/read.php
+++ admin/phpscripts/read.php
@@ -28,8 +28,20 @@ function getSingle($tbl, $col, $id){
 	mysqli_close($link);
 }
 
-function filterType($tbl){
-	include 'connect.php';
-	$queryFilter = "SELECT * FROM tbl_movies m, tbl_genre g, tbl_mov_genre mg WHERE m.movies_id = mg.movies_id AND g.genre_id = mg.genre_id AND g.genre_id = 1"
+function filterType($tbl, $tbl2, $tbl3, $col, $col2, $col3, $filter){
+	include('connect.php');
+
+
+
+	$queryFilter = "SELECT * FROM {$tbl}, {$tbl2}, {$tbl3} WHERE {$tbl}.{$col} = {$tbl3}.{$col} AND {$tbl2}.{$col2} = {$tbl3}.{$col2} AND {$tbl2}.{$col3} = '{$filter}'";
+	// echo $queryFilter;
+	$runFilter = mysqli_query($link, $queryFilter);
+	if($runFilter){
+		return $runFilter;
+	}else{
+		$error = "We can't find those movies right now";
+		return $error;
+	}
+	mysqli_close($link);
 }
 ?>
--- index.php
+++ index.php
@@ -4,8 +4,14 @@
 
   require_once('admin/phpscripts/config.php');
   if(isset($_GET['filter'])){
+    $tbl = "tbl_movies";
+  	$tbl2 = "tbl_genre";
+  	$tbl3 = "tbl_mov_genre";
+  	$col = "movies_id";
+  	$col2 = "genre_id";
+  	$col3 = "genre_name";
     $filter = $_GET['filter'];
-    $getMovies = filterType();
+    $getMovies = filterType($tbl, $tbl2, $tbl3, $col, $col2, $col3, $filter);
   }else{
   $tbl = 'tbl_movies';
   $getMovies = getAll($tbl);
