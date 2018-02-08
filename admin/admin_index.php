9db80c07af066d0100e328253e3aa679055c2b75
Vitez <p_vitez@fanshawec.ca>
2018-02-08 10:18:26 -0500

    add partial login

---
+++ admin/admin_index.php
@@ -0,0 +1,23 @@
+<?php
+  // ini_set('display_errors',1);
+  // error_reporting(E_ALL);
+
+  require_once('phpscripts/config.php');
+
+  
+
+ ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>OMS Portal Login</title>
+</head>
+<body>
+  <h1>Welcome to Zombocom</h1>
+
+</body>
+</html>
